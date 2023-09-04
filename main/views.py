2e5ce36b71d7a89719487a071f655a322eab5927
willymwaura <wilsonmwaura697@gmail.com>
2023-09-04 10:18:32 +0300

    adding stk push

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ from requests.auth import HTTPBasicAuth
 import json
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 
 
 def homepage(request):
